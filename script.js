182babe41ac1119a33ab509abab462647cf8a0f7
MATHEUS RAMON ORLANDI ROCHA <orlandi.matheus@gmail.com>
2026-08-12 13:22:15 -0300

    Alterado imagens de avatar e links para arquivos pessoais , como instruido no curso.

--- script.js
+++ script.js
@@ -10,10 +10,10 @@ function toggleMode() {
 
     if(html.classList.contains('light')) {
 
-    img.setAttribute('src', './assets/avatar-light.png')
+    img.setAttribute('src', './assets/meu-avatar-light.png')
 
     } else {
 
-    img.setAttribute('src', './assets/avatar.png')
+    img.setAttribute('src', './assets/meu-avatar.png')
     }
 }
